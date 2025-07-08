04312e659c3fa6b410859b8dab7e1d94bfdd5878
pvanil13 <95075331+pvanil13@users.noreply.github.com>
2025-07-08 17:30:36 +0530

    Update app.py

--- src/authorizer/app.py
+++ src/authorizer/app.py
@@ -1,17 +1,15 @@
-import boto3, json, os
-from uuid import uuid4
-
-dynamodb = boto3.resource('dynamodb')
-sns = boto3.client('sns')
-table = dynamodb.Table(os.environ['TABLE_NAME'])
-topic_arn = os.environ['TOPIC_ARN']
-
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    item = {
-        'objectId': str(uuid4()),
-        'data': body.get('data', 'No data')
-    }
-    table.put_item(Item=item)
-    sns.publish(TopicArn=topic_arn, Message=json.dumps(item))
-    return {"statusCode": 200, "body": json.dumps(item)}
+    token = event['authorizationToken']
+    if token == "allow123":
+        return {
+            "principalId": "user",
+            "policyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Action": "execute-api:Invoke",
+                    "Effect": "Allow",
+                    "Resource": event['methodArn']
+                }]
+            }
+        }
+    raise Exception("Unauthorized")
